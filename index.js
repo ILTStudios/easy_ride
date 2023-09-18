e6a7629eaa843431e85e1cbfa165b11ff42edaf1
Unknown <iltstudios69@gmail.com>
2023-09-18 19:49:14 +0100

    made sign in and log in components with scss components

--- index.js
+++ index.js
@@ -16,7 +16,38 @@ express_app.use(body_parser.urlencoded({
     extended: true,
 }));
 
+express_app.listen(4000, () => {
+    console.log("Server is Listening to Port: 4000");
+});
+
 //mongoDB
 const { Schema } = mongoose;
 mongoose.Promise = global.Promise;
 
+mongoose.connect('mongodb+srv://iltstudios:iltstudios@fjord.kqqq4ms.mongodb.net/')
+var db = mongoose.connection;
+
+db.on('error', () => console.log('Mongoose had trouble connecting'));
+db.once('open', () => console.log('Connection Successful'));
+
+var user_schema = new Schema({
+    email: String,
+    password: String,
+});
+
+var easyride = mongoose.models.user || mongoose.model('users', user_schema);
+easyride.find({}, (err, data) => {
+    if(err){console.log(err)}
+    else{
+        console.log(data);
+    }
+});
+express_app.get('/', (req, res) => {
+    console.log('beans')
+    easyride.find({}, (err, data) => {
+        if(err){console.log(err)}
+        else{
+            console.log(data);
+        }
+    });
+});
