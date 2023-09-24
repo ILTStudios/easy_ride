cfa719ab4f3439f56ff33b553f7a1ea89f2bd1ec
Unknown <iltstudios69@gmail.com>
2023-09-24 17:15:46 +0100

    Developed Sign and Log Frontend GUI

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const express_app = express();
 
-const mongoose = require('mongoose');
 const body_parser = require('body-parser');
 
 //set express settings
@@ -21,33 +20,60 @@ express_app.listen(4000, () => {
 });
 
 //mongoDB
-const { Schema } = mongoose;
-mongoose.Promise = global.Promise;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://iltstudios:iltstudios@fjord.kqqq4ms.mongodb.net/easy_ride?retryWrites=true&w=majority";
 
-mongoose.connect('mongodb+srv://iltstudios:iltstudios@fjord.kqqq4ms.mongodb.net/')
-var db = mongoose.connection;
-
-db.on('error', () => console.log('Mongoose had trouble connecting'));
-db.once('open', () => console.log('Connection Successful'));
-
-var user_schema = new Schema({
-    email: String,
-    password: String,
+const client = new MongoClient(uri, {
+  // serverApi: {
+  //   version: ServerApiVersion.v1,
+  //   strict: true,
+  //   deprecationErrors: true,
+  // }
 });
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+    var users = await client.db().collection('users').find().toArray();
+    console.log(users);
 
-var easyride = mongoose.models.user || mongoose.model('users', user_schema);
-easyride.find({}, (err, data) => {
-    if(err){console.log(err)}
-    else{
-        console.log(data);
-    }
-});
-express_app.get('/', (req, res) => {
-    console.log('beans')
-    easyride.find({}, (err, data) => {
-        if(err){console.log(err)}
-        else{
-            console.log(data);
-        }
-    });
-});
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// const { Schema } = mongoose;
+// mongoose.Promise = global.Promise;
+
+// mongoose.connect('mongodb+srv://iltstudios:iltstudios@fjord.kqqq4ms.mongodb.net/')
+// var db = mongoose.connection;
+
+// db.on('error', () => console.log('Mongoose had trouble connecting'));
+// db.once('open', () => console.log('Connection Successful'));
+
+// var user_schema = new Schema({
+//     email: String,
+//     password: String,
+// });
+
+// var easyride = mongoose.models.user || mongoose.model('users', user_schema);
+// easyride.find({}, (err, data) => {
+//     if(err){console.log(err)}
+//     else{
+//         console.log(data);
+//     }
+// });
+// express_app.get('/', (req, res) => {
+//     console.log('beans')
+//     easyride.find({}, (err, data) => {
+//         if(err){console.log(err)}
+//         else{
+//             console.log(data);
+//         }
+//     });
+// });
