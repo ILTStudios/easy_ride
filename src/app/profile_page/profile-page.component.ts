81457db71d1d6a8084707bd628648601e139a4da
Unknown <iltstudios69@gmail.com>
2023-10-11 21:03:25 +0100

    added ability to switch pages

--- src/app/profile_page/profile-page.component.ts
+++ src/app/profile_page/profile-page.component.ts
@@ -5,4 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.scss']
 })
-export class ProfilePageComponent {}
+export class ProfilePageComponent {
+  change_setting(setting_page:String){
+    document.querySelector(`.${setting_page}`)?.classList.add('on');
+
+    if(setting_page == 'general_setting'){
+      document.querySelector('.general_setting')?.classList.add('on');
+      document.querySelector('.change_filter > .selection')?.classList.add('general');
+      document.querySelector('.change_filter > .selection')?.classList.remove('main');
+    }else{
+      document.querySelector('.general_setting')?.classList.remove('on');
+    }
+
+    if(setting_page == 'main_setting'){
+      document.querySelector('.main_setting')?.classList.add('on');
+      document.querySelector('.change_filter > .selection')?.classList.add('main');
+      document.querySelector('.change_filter > .selection')?.classList.remove('general');
+    }else{
+      document.querySelector('.main_setting')?.classList.remove('on');
+    }
+  }
+}
